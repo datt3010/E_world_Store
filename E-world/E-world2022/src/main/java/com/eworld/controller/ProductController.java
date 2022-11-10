5228348e4e5b8c4033adb3d3efe61e73526d1b6a
Tấn Đạt <dat.huynh1@onemount.com>
2022-11-10 17:00:27 +0700

    main: .

--- E-world/E-world2022/src/main/java/com/eworld/controller/ProductController.java
+++
@@ -1,8 +0,0 @@
-package com.eworld.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ProductController {
-
-}
--- E-world/E-world2022/src/main/java/com/eworld/service/ProductSerivce.java
+++
@@ -1,5 +0,0 @@
-package com.eworld.service;
-
-public class ProductSerivce {
-
-}
