72054a91d2565c83aef15758aced2124625fb7d2
Tấn Đạt <dat.huynh1@onemount.com>
2022-11-25 19:10:24 +0700

    .

--- E-world/E-world2022/src/main/java/com/eworld/controller/HomeController.java
+++ E-world/E-world2022/src/main/java/com/eworld/controller/HomeController.java
@@ -1,15 +1,36 @@
 package com.eworld.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.eworld.dto.ProductDto;
+import com.eworld.filter.ProductFilter;
+import com.eworld.service.ProductService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping()
-	public String home(Model model) {
+	public String home(Model model,@RequestParam(name="keyword", required = false)String keyword) {
+Pageable pageable = PageRequest.of(0,4,Direction.ASC,"name");
+		
+		ProductFilter filter = ProductFilter.builder()
+				.keyword(keyword)
+				.build();
+		Page<ProductDto> listProduct = productService.findPaging(filter, pageable);
+		model.addAttribute("listProduct", listProduct);
+		
 		return "user/home/Index";
 	}
 	
@@ -27,12 +48,7 @@ public class HomeController {
 	public String login() {
 		return "/user/login/login";
 	}
-	
-	@GetMapping("sanpham")
-	public String product() {
-		return "user/product/Listproduct";
-	}
-	
+
 	@GetMapping("chitietsanpham")
 	public String detailProduct() {
 		return "user/product/ProductDetail";
